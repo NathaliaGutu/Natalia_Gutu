8eb8060a0e6c3ca95c1dded544680e3fc81cfad7
NathaliaGutu <nataliagutupaun@gmail.com>
2022-07-31 15:25:55 +0300

    sarcina2 cu operatii

---
+++ Sarcina2.java
@@ -0,0 +1,37 @@
+public class Sarcina2 {
+    public int sumaNumer(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int numPare(int n) {
+        for (int a = 0; a <= n; a++) {
+            if (a % 2 == 0) ;
+            {
+                System.out.println(a + " ");
+            }
+            return a++;
+        }
+
+        public String hello (int a, int b) {
+            int a = 1;
+            int b = 10;
+            for (a = 1; a <= b; a++)
+                System.out.println("Hello");
+        }
+        return hello;
+    }
+
+    public int numImpare(int n) {
+        for (int a = 0; a <= n; a++) {
+            if (a % 2 != 0) ;
+            {
+                System.out.println(" ");
+            }
+            return a++;
+        }
+    }
+}
