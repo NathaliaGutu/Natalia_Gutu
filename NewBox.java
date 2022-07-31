c6ba5c8886bad343c955bac92936a2c531e020d6
NathaliaGutu <nataliagutupaun@gmail.com>
2022-07-31 15:24:51 +0300

    new box

---
+++ NewBox.java
@@ -0,0 +1,10 @@
+public class NewBox {
+    public static void main(String[] args){
+        Box myBox = new Box();
+        myBox.name="Raffaello";
+        myBox.colour="AlbRosu";
+        myBox.weight=10;
+
+        System.out.println("In contructor");
+    }
+}
