7b971a297260afa2a2b20d0b6665dd55294a638b
NathaliaGutu <nataliagutupaun@gmail.com>
2022-08-03 07:50:33 +0300

    string actualizat

--- StringInversat.java
+++ StringInversat.java
@@ -4,9 +4,9 @@ public class StringInversat {
     public static void main(String[] args) {
         String string1 = new String("tak");
 
-        System.out.println(string1.charAt(string1.length() - 1));
-        System.out.println(string1.charAt(string1.length() - 2));
-       System.out.println(string1.charAt(string1.length() - 3));
+        System.out.print(string1.charAt(string1.length() - 1));
+        System.out.print(string1.charAt(string1.length() - 2));
+       System.out.print(string1.charAt(string1.length() - 3));
 
         // System.out.println(string1.replace("tak", "kat"));
 
