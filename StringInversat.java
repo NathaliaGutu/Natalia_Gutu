7344569b28e1dfcf86c87b3be4ffe142d47f1edb
NathaliaGutu <nataliagutupaun@gmail.com>
2022-08-03 13:33:54 +0300

    String inversat actualizat

--- StringInversat.java
+++ StringInversat.java
@@ -2,13 +2,12 @@ package stringInversat;
 
 public class StringInversat {
     public static void main(String[] args) {
-        String string1 = new String("tak");
-
-        System.out.print(string1.charAt(string1.length() - 1));
-        System.out.print(string1.charAt(string1.length() - 2));
-       System.out.print(string1.charAt(string1.length() - 3));
-
-        // System.out.println(string1.replace("tak", "kat"));
-
+        String str1 = "Tekwill";
+        int len= str1.length();
+        String reverse= " ";
+        for (int i = len -1; i >= 0; i--){
+            reverse = reverse + str1.charAt(i);
+        }
+        System.out.println(reverse);
     }
 }
