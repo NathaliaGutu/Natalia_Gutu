07b6eb94d38c1a421ea2291935b48bd1c6d437b4
NathaliaGutu <nataliagutupaun@gmail.com>
2022-08-08 14:13:50 +0300

    arata indexul elementului din array

---
+++ MyArray.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class MyArray {
+    public static void main(String[]args){
+        int[] arr={15,17,19};
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[3]);
+        }
+    }
+}
